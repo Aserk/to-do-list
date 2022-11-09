8fc9283817733fc3bd6b6a22caea1ed2a4a8a104
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-09 23:35:23 +0100

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,14 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -19,7 +27,9 @@
                     <button class="list__button list__button--done">
                         ${task.done ? "✔" : ""}
                     </button>
-                    <span  ${task.done ? "class=\"list__text--throughLine\"" : ""}>${task.content}</span>
+                    <span  ${task.done ? "class=\"list__text--throughLine\"" : ""}>
+                        ${task.content}
+                    </span>
                     <button class="list__button list__button--remove">🗑</button>
                 </li>
             `;
@@ -28,8 +38,24 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
