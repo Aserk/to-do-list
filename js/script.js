125bbd407f28c405cd6901aaa9dfcc013c2c4902
Rafał Jabłoński <asserk.codes@gmail.com>
2022-11-10 06:24:06 +0100

    binding events move to a separate function

--- js/script.js
+++ js/script.js
@@ -28,6 +28,24 @@
         render();
     };
 
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toogleDoneButtons = document.querySelectorAll(".js-done");
+
+        toogleDoneButtons.forEach((toogleDoneButton, index) => {
+            toogleDoneButton.addEventListener("click", () => {
+                toogleDoneTask(index);
+            });
+        });
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -47,21 +65,7 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
-        });
-
-        const toogleDoneButtons = document.querySelectorAll(".js-done");
-
-        toogleDoneButtons.forEach((toogleDoneButton, index) => {
-            toogleDoneButton.addEventListener("click", () => {
-                toogleDoneTask(index);
-            });
-        });
+        bindEvents();
     };
 
     const onFormSubmit = (event) => {
